f31319e93f39a966d13ba93c4d81ee27a60c645b
philippmeissner <pm@zeit.io>
2018-03-04 15:44:21 +0100

    fixed regexp

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,15 +1,13 @@
 class Phrase
   def initialize(phrase)
-    # Also remove apostrophes that do NOT surround words
-    # eg "can't" should remain "can't" but "'large'" should become "large"
-    @phrase = phrase.downcase.gsub(/[^a-zA-Z' ]/i, ' ')
+    @phrase = phrase.downcase.scan(/\b[\w']+\b/)
 
   end
 
   def word_count
     word_counts = Hash.new 0
 
-    @phrase.split.each do |word|
+    @phrase.each do |word|
       word_counts[word] += 1
     end
     word_counts
